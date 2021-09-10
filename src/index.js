ff7664990c7f4435130fe7d6ce4b4357789c467f
lotuswalking <jiangzhonping@163.com>
2021-09-10 09:16:19 -0400

    update with bootstraps

--- src/index.js
+++ src/index.js
@@ -24,17 +24,17 @@ class Board extends React.Component {
   render() {
     return (
       <div>
-        <div className="board-row">
+        <div className="row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
           {this.renderSquare(2)}
         </div>
-        <div className="board-row">
+        <div className="row">
           {this.renderSquare(3)}
           {this.renderSquare(4)}
           {this.renderSquare(5)}
         </div>
-        <div className="board-row">
+        <div className="row">
           {this.renderSquare(6)}
           {this.renderSquare(7)}
           {this.renderSquare(8)}
@@ -77,7 +77,7 @@ class Game extends React.Component {
     jumpTo(step) {
       this.setState({
         stepNumber: step,
-        xIsNext: (step % 2) ==0,
+        xIsNext: (step % 2) ===0,
       })
     }
   render() {
